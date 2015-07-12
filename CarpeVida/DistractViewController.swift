ffe081ec5387564d896a9d459fd4fc661a9d8190
Angie Wang <angiewang123@gmail.com>
2015-07-12 11:20:11 -0700

    Updated version

---
+++ CarpeVida/DistractViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DistractViewController.swift
+//  CarpeVida
+//
+//  Created by Jason Cheung on 2015-07-11.
+//  Copyright (c) 2015 Angie Wang. All rights reserved.
+//
+
+import UIKit
+
+class DistractViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
